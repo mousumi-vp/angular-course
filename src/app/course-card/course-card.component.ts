d8f16e2f3c350e7189007777cdaaccefa569e6c5
mousumi-vp <mousumipriyadarsinis@gmail.com>
2024-09-04 14:36:56 +0530

    angular-course

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'course-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './course-card.component.html',
+  styleUrl: './course-card.component.css'
+})
+export class CourseCardComponent implements OnInit {
+  constructor(){}
+  ngOnInit(){
+  }
+
+}
